e8c3bd44591083ebd1128907bcaade1fb7a7f9da
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-06-10 10:49:56 +0200

    animated section work

--- .next/static/webpack/webpack.4f965d719c5c43ad.hot-update.js
+++ .next/static/webpack/webpack.4f965d719c5c43ad.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "052a73f85dc08f0e"; }
+/******/ 	__webpack_require__.h = function() { return "adc21714fded4632"; }
 /******/ }();
 /******/ 
 /******/ }
--- .next/static/webpack/webpack.adc21714fded4632.hot-update.js
+++ .next/static/webpack/webpack.adc21714fded4632.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c9a1d40fac707384"; }
+/******/ 	__webpack_require__.h = function() { return "53585848803f63f2"; }
 /******/ }();
 /******/ 
 /******/ }
