24b95dc2a157b998a45ac67eaa61766df1f74e33
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-06-10 10:23:12 +0200

    add profile picture

--- .next/static/webpack/webpack.efb32db41c5914ea.hot-update.js
+++ .next/static/webpack/webpack.efb32db41c5914ea.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d7ea808954ced0f3"; }
+/******/ 	__webpack_require__.h = function() { return "8a18a343d901f5ad"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, Tchitdjian Lucas a front-end developer based in France
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -12,6 +24,23 @@ const Page = () => {
             Lucas Tchitdjian
           </Heading>
           <p>Front-end developpeur</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/profil-picture.jpg"
+            alt="profile picture"
+          />
         </Box>
       </Box>
     </Container>
