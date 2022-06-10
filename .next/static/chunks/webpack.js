a3331939f623be70d5899a67bebfec069b71ce15
Lucas TCHITDJIAN <tchitdjian.lucas@gmail.com>
2022-06-10 11:55:27 +0200

    animate dark mode button

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26681a5252030199"; }
+/******/ 		__webpack_require__.h = function() { return "7c688a7bebed6ba0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
